4569ad73cad6817838d7ffcae5a0e8d8ca296623
Mytyl <github.aeyip@simplelogin.com>
2024-11-21 00:16:01 -0500

    redoing UI

--- src/paint_app/Main.java
+++ src/paint_app/Main.java
@@ -2,15 +2,10 @@ package paint_app;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
-import paint_app.components.Tools;
-import paint_app.components.Workspace;
 
 /* TODO
  * toolbox
@@ -39,50 +34,58 @@ public class Main extends Application {
         launch(args);
     }
 
-    public static Rectangle makePlaceholder(int w, int h, Color c) {
-        var rect = new Rectangle();
-        rect.setWidth(w);
-        rect.setHeight(h);
-        rect.setFill(c);
-        return rect;
-    }
-
     @Override
     public void start(Stage stage) {
         var root = new VBox();
-        root.getStylesheets().add("style.css");
-        root.setBackground(new Background(new BackgroundFill(InterfaceColors.Base, null, null)));
-        root.setMinSize(Globals.WIDTH, Globals.HEIGHT);
-
+//        root.getStylesheets().add("style.css");
+//        root.setBackground(new Background(new BackgroundFill(InterfaceColors.Base, null, null)));
+//        root.setMinSize(Globals.WIDTH, Globals.HEIGHT);
+//
         var scene = new Scene(root, Globals.WIDTH, Globals.HEIGHT);
 
-        var top_panel = new Tools();
-        top_panel.minWidthProperty().bind(root.widthProperty());
-        top_panel.maxWidthProperty().bind(root.widthProperty());
+        Rectangle top_panel = new Rectangle();
+        top_panel.widthProperty().bind(root.widthProperty());
+        top_panel.heightProperty().bind(root.heightProperty().multiply(0.25));
+        top_panel.setFill(Color.LIGHTBLUE);
 
-        var bottom_panel = new HBox();
-        bottom_panel.setSpacing(10);
-        bottom_panel.minWidthProperty().bind(root.widthProperty());
-        bottom_panel.maxWidthProperty().bind(root.widthProperty());
-        bottom_panel.minHeightProperty().bind(root.heightProperty());
-        bottom_panel.maxHeightProperty().bind(root.heightProperty());
+        Rectangle bottom_panel = new Rectangle();
+        bottom_panel.widthProperty().bind(root.widthProperty());
+        bottom_panel.heightProperty().bind(root.heightProperty().multiply(0.75));
+        bottom_panel.setFill(Color.LIGHTGREEN);
 
-        var workspace = new Workspace();
-        workspace.minWidthProperty().bind(bottom_panel.widthProperty().subtract(210));
-        workspace.minHeightProperty().bind(bottom_panel.heightProperty());
-
-        var dock = makePlaceholder(200, 100, Color.PALEGREEN);
-        dock.minWidth(200);
-        dock.maxWidth(200);
-        dock.heightProperty().bind(bottom_panel.heightProperty());
-
+        // Add the panels to the VBox
         root.getChildren().addAll(top_panel, bottom_panel);
-        bottom_panel.getChildren().addAll(workspace, dock);
+
+
+//        var top_panel = new Tools();
+//        top_panel.minWidthProperty().bind(root.widthProperty());
+//        top_panel.maxWidthProperty().bind(root.widthProperty());
+//
+//        var bottom_panel = new HBox();
+//        bottom_panel.setSpacing(10);
+//        bottom_panel.minWidthProperty().bind(root.widthProperty());
+//        bottom_panel.maxWidthProperty().bind(root.widthProperty());
+//        bottom_panel.minHeightProperty().bind(root.heightProperty());
+//        bottom_panel.maxHeightProperty().bind(root.heightProperty());
+//
+//        var workspace = new Workspace();
+//        workspace.minWidthProperty().bind(bottom_panel.widthProperty().subtract(210));
+//        workspace.minHeightProperty().bind(bottom_panel.heightProperty());
+//
+//        var dock = makePlaceholder(200, 100, Color.PALEGREEN);
+//        dock.minWidth(200);
+//        dock.maxWidth(200);
+//        dock.heightProperty().bind(bottom_panel.heightProperty());
+//
+//        root.getChildren().addAll(top_panel, bottom_panel);
+//        bottom_panel.getChildren().addAll(workspace, dock);
+//
 
         stage.setScene(scene);
         stage.setTitle("Paint App");
-        stage.setMinWidth(Globals.WIDTH);
-        stage.setMinHeight(Globals.HEIGHT);
+        stage.setWidth(Globals.WIDTH);
+        stage.setHeight(Globals.HEIGHT);
+        stage.setResizable(false);
         stage.show();
     }
 }
--- src/paint_app/components/Tools.java
+++ src/paint_app/components/Tools.java
@@ -1,91 +1,98 @@
 package paint_app.components;
 
 import javafx.geometry.Insets;
-import javafx.geometry.Orientation;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.Separator;
-import javafx.scene.layout.*;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.*;
-import paint_app.InterfaceColors;
+import javafx.scene.shape.Circle;
 
-import java.util.EnumMap;
 import java.util.function.Function;
 
 public class Tools extends HBox {
-
-    static final String[] colors = {
-            "#2E2E2E", "#7F7F7F",
-            "#4D4D4D", "#A3A3A3",
-            "#FF4D4D", "#FF9999",
-            "#FF7F7F", "#FFB3B3",
-            "#FFB366", "#FFEB99",
-            "#FF9900", "#FFCC66",
-            "#4D9933", "#80E080",
-            "#66B266", "#B3FFB3",
-            "#6666FF", "#B3B3FF",
-            "#4D66CC", "#99CCFF"
+    static final String[] COLORS = {
+            "#000000", "#FFFFFF", "#FF0000", "#00FF00",
+            "#0000FF", "#FFFF00", "#FF00FF", "#00FFFF",
+            "#D3D3D3", "#A9A9A9", "#FF6347", "#90EE90",
+            "#ADD8E6", "#FFFFE0", "#FFB6C1", "#E0FFFF",
+            "#8B0000", "#006400", "#00008B", "#B8860B",
+            "#8B008B", "#008B8B", "#FFA500", "#FFC0CB",
+            "#A52A2A", "#8A2BE2", "#808000", "#D2B48C"
     };
-    // TODO: replace with custom shape type & maybe add hover text
-    // FIXME: https://stackoverflow.com/questions/67607416/what-is-the-best-way-to-statically-initialize-an-enummap-in-java don't keep it as is
-    static final EnumMap<InterfaceShape, Shape> shapes = new EnumMap<>(InterfaceShape.class);
-
-    static {
-        final var poly = new Polygon(0, 0, -35, 0, -35.0 / 2, -35, 0, 0);
-        final var arrow = new Polyline(0, 0, 0, -25, -5, -20, 0, -25, 5, -20);
-
-        shapes.put(InterfaceShape.CIRCLE, new Circle(18, Color.WHITESMOKE));
-        shapes.put(InterfaceShape.RECTANGLE, new Rectangle(36, 36, Color.WHITESMOKE));
-        shapes.put(InterfaceShape.POLYGON, poly);
-
-        for (var s : shapes.values()) {
-//            s.setStroke(InterfaceColors.Crust);
-            s.setFill(Color.WHITESMOKE);
-//            s.setStrokeWidth(2);
-        }
-
-        shapes.put(InterfaceShape.ARROW, arrow);
-
-        arrow.setStrokeWidth(5);
-        arrow.setStroke(Color.WHITESMOKE);
-        arrow.setStrokeLineJoin(StrokeLineJoin.ROUND);
-    }
 
     public Tools() {
-        getStyleClass().add("toolbox-label");
-        setAlignment(Pos.TOP_CENTER);
-        setSpacing(30);
-        setPadding(new Insets(20, 20, 20, 0));
-        setBackground(new Background(new BackgroundFill(Color.web("#363a4f"), null, null)));
 
-        var brush_grid = gridHelper("brushes", 0, i -> {
-            return new Rectangle();
-
+        var colors = gridHelper("color picker", COLORS.length, i -> {
+            var btn = new Button();
+            var c = new Circle(12);
+            c.setFill(Color.web(COLORS[i]));
+            btn.setShape(c);
+            return btn;
         });
-
-        var shape_grid = gridHelper("shapes", shapes.size(), i -> {
-            final var k = InterfaceShape.values()[i];
-            return shapes.get(k);
-        });
-
-        var color_picker = gridHelper("color picker", colors.length, i -> {
-            var circle = new Circle(12, Color.web(colors[i]));
-            circle.setStroke(InterfaceColors.Crust);
-            circle.setStrokeWidth(2);
-            return circle;
-        });
-
-        var children = getChildren();
-        children.addAll(brush_grid, shape_grid, color_picker);
-
-        for (int i = getChildren().size() - 1; i > 0; i--) {
-            var sep = new Separator(Orientation.VERTICAL);
-            children.add(i, sep);
-        }
     }
+//    // TODO: replace with custom shape type & maybe add hover text
+//    // FIXME: https://stackoverflow.com/questions/67607416/what-is-the-best-way-to-statically-initialize-an-enummap-in-java don't keep it as is
+//    static final EnumMap<InterfaceShape, Shape> shapes = new EnumMap<>(InterfaceShape.class);
+//
+//    static {
+//        final var poly = new Polygon(0, 0, -35, 0, -35.0 / 2, -35, 0, 0);
+//        final var arrow = new Polyline(0, 0, 0, -25, -5, -20, 0, -25, 5, -20);
+//
+//        shapes.put(InterfaceShape.CIRCLE, new Circle(18, Color.WHITESMOKE));
+//        shapes.put(InterfaceShape.RECTANGLE, new Rectangle(36, 36, Color.WHITESMOKE));
+//        shapes.put(InterfaceShape.POLYGON, poly);
+//
+//        for (var s : shapes.values()) {
+////            s.setStroke(InterfaceColors.Crust);
+//            s.setFill(Color.WHITESMOKE);
 
+    /// /            s.setStrokeWidth(2);
+//        }
+//
+//        shapes.put(InterfaceShape.ARROW, arrow);
+//
+//        arrow.setStrokeWidth(5);
+//        arrow.setStroke(Color.WHITESMOKE);
+//        arrow.setStrokeLineJoin(StrokeLineJoin.ROUND);
+//    }
+//
+//    public Tools() {
+//        getStyleClass().add("toolbox-label");
+//        setAlignment(Pos.TOP_CENTER);
+//        setSpacing(30);
+//        setPadding(new Insets(20, 20, 20, 0));
+//        setBackground(new Background(new BackgroundFill(Color.web("#363a4f"), null, null)));
+//
+//        var brush_grid = gridHelper("brushes", 0, i -> {
+//            return new Rectangle();
+//
+//        });
+//
+//        var shape_grid = gridHelper("shapes", shapes.size(), i -> {
+//            final var k = InterfaceShape.values()[i];
+//            return shapes.get(k);
+//        });
+//
+//        var color_picker = gridHelper("color picker", colors.length, i -> {
+//            var circle = new Circle(12, Color.web(colors[i]));
+//            circle.setStroke(InterfaceColors.Crust);
+//            circle.setStrokeWidth(2);
+//            return circle;
+//        });
+//
+//        var children = getChildren();
+//        children.addAll(brush_grid, shape_grid, color_picker);
+//
+//        for (int i = getChildren().size() - 1; i > 0; i--) {
+//            var sep = new Separator(Orientation.VERTICAL);
+//            children.add(i, sep);
+//        }
+//    }
+//
     static VBox gridHelper(String label_text, int size, Function<Integer, Node> generator) {
         var box = new VBox();
         box.setAlignment(Pos.CENTER);
@@ -96,10 +103,6 @@ public class Tools extends HBox {
 
         for (int i = 0; i < size; i++) {
             var node = generator.apply(i);
-            if (node == null) {
-                System.out.printf("found null at [name=%s,shape=%s]\n", label_text, InterfaceShape.values()[i]);
-                continue;
-            }
 
             if (size <= 10) {
                 grid.add(node, i, 0);
@@ -115,8 +118,8 @@ public class Tools extends HBox {
         return box;
     }
 
-    enum InterfaceShape {
-        CIRCLE, RECTANGLE, POLYGON, ARROW
-
-    }
+//    enum InterfaceShape {
+//        CIRCLE, RECTANGLE, POLYGON, ARROW
+//
+//    }
 }
